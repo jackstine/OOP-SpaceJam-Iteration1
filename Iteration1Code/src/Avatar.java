fa8bbda47492f7012ce1d35b205ea2b8f4bd2c08
tgittelmacher <tgittelmacher@ufl.edu>
2015-02-09 16:42:13 -0500

    Avatar and Entity
    skeletons for Avatar and Entity

---
+++ Iteration1Code/src/Avatar.java
@@ -0,0 +1,24 @@
+
+public class Avatar extends Entity{
+	private Inventory inventory;
+	
+	public Avatar() {
+		
+	}
+	
+	/*
+	//private void attack() {
+	//	
+	//}
+	//may not need to override
+	 */
+	
+	/*
+	//private void useAbility() {
+	//	
+	//}
+	//may not need to override
+	 */
+	
+	
+}
---
+++ Iteration1Code/src/Entity.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+
+
+public abstract class Entity {
+	//private Map<String,Stat> stats; 	// UNCOMMENT ONCE MADE
+	//private GameMap map;				// UNCOMMENT ONCE MADE
+	//private Location location; 		// UNCOMMENT ONCE MADE
+	//private Equipment equipment;		// BLAH
+	//private Occupation occupation;	// BLAH
+	
+	private void attack() {
+		//occupation.attack();
+	}
+	
+	private void useAbility() {
+		//occupation.useAbility();
+	}
+	
+	public Location getLocation() {
+		//query map for location
+	}
+}
