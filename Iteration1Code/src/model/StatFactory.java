92a6fb4c57333f1e81f3a6dd7d5f86a7c65753b9
tgittelmacher <tgittelmacher@ufl.edu>
2015-02-09 18:00:47 -0500

    StatFactory
    adding dynamic constructors for initializing stats

--- Iteration1Code/src/model/StatFactory.java
+++ Iteration1Code/src/model/StatFactory.java
@@ -22,37 +22,41 @@ public abstract class StatFactory {
 	}
 	
 	protected void createStats() {
-		
+		map.put("Lives", new Lives(getLivesLeft()));
+		map.put("Strength", new Strength(getStrength()));
+		map.put("Agility", new Agility(getAgility()));
+		map.put("Intellect", new Intellect(getIntellect()));
+		map.put("Hardiness", new Hardiness(getHardiness()));
+		map.put("Experience", new Experience(getExperience()));
+		map.put("Movement", new Movement(getMovement()));
 	}
 	
 	protected int getLivesLeft() {
-		
+		return 3;
 	}
 	
 	protected int getStrength() {
-		
+		return 10;
 	}
 	
 	protected int getAgility() {
-		
+		return 10;
 	}
 	
 	protected int getIntellect() {
-		
+		return 10;
 	}
 	
 	protected int getHardiness() {
-		
+		return 10;
 	}
 	
 	protected int getExperience() {
-		
+		return 0;
 	}
 	
 	protected int getMovement() {
-		
+		return 10;
 	}
-	
-	
 	
 }
