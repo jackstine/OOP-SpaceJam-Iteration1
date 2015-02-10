89471ae0be97555656a34740e4da9b730949de7e
Juan Nunez <njuan1993@yahoo.com>
2015-02-09 20:53:56 -0500

    Updated Map and Tile

--- Iteration1Code/src/model/GameMap.java
+++ Iteration1Code/src/model/GameMap.java
@@ -1,6 +1,24 @@
 package model;
+import java.util.*;
 
 public class GameMap{
-	
-	
+	private Tile[][] map;
+	private HashMap<Entity,Location> entityToLocationMap;
+	private HashMap<Item,Location> itemToLocationMap;
+
+
+	public GameMap(){
+
+	}
+
+	public Tile getTile(Location location){
+		//returns tile;
+	}
+
+	public Tile getEntityTile(Entity entity){
+		//returns a tile;
+	}
+	public Item getItemTile(Item item){
+		//returns a tile;
+	}
 }
--- Iteration1Code/src/model/Tile.java
+++ Iteration1Code/src/model/Tile.java
@@ -1,5 +1,23 @@
 package model;
 
 public class Tile{
-	
+	private Terrain terrain;
+	private Location location;
+	private Item item;
+	private Entity entity;
+	private AreaEffect areaEffect;
+	private Decal decal;
+
+	public Tile(){
+
+	}
+
+	public Tile getTile(){
+		return this;
+	}
+
+	public boolean isPassable(){
+		//fill this in later
+	}
+
 }
