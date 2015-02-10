374d536bda3e0c4a088548fb91cbea09faa6cea7
Juan Nunez <njuan1993@yahoo.com>
2015-02-10 13:03:32 -0500

    Some updates

--- Iteration1Code/src/model/RadioactiveWasteTerrain.java
+++ Iteration1Code/src/model/RadioactiveWasteTerrain.java
@@ -1,9 +1,14 @@
 package model;
 
 public class RadioactiveWasteTerrain extends Terrain{
+	private boolean passable;
+
+	public RadioactiveWasteTerrain(){
+		passable=false;
+	}
 
 	public boolean isPassable(){
-		return false;
+		return passable;
 	}
 
 }
--- Iteration1Code/src/model/Tile.java
+++ Iteration1Code/src/model/Tile.java
@@ -3,21 +3,34 @@ package model;
 public class Tile{
 	private Terrain terrain;
 	private Location location;
-	private Item item;
-	private Entity entity;
-	private AreaEffect areaEffect;
-	private Decal decal;
+	//private Item item;
+	//private Entity entity;
+	//private AreaEffect areaEffect;
+	//private Decal decal;
 
-	public Tile(){
+	public Tile(Terrain terrain, int x,int y){ //will take more parameters
+		this.terrain=terrain;
+		location= new Location(x,y);
 
 	}
-
-	public Tile getTile(){
+	/*
+	public Tile getTile(){  //not sure if this is needed. Leaving it here for later
 		return this;
 	}
+	*/ 
 
 	public boolean isPassable(){
-		//fill this in later
+		return terrain.isPassable();
+	}
+	//will probably need to add a setTerrain for the purpose
+	//of making the map 
+ 
+	public void setTerrain(Terrain ter){  
+		terrain=ter;
+	}
+
+	public Terrain getTerrain(){
+		return terrain;
 	}
 
 }
