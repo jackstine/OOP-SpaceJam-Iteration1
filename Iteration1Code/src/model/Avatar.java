e105a0b37a7c034c2887f06c85d4e497b148ea37
Juan <njuan1993@yahoo.com>
2015-02-14 17:12:30 -0500

    Close one. phew!

--- Iteration1Code/src/model/Avatar.java
+++ Iteration1Code/src/model/Avatar.java
@@ -1,84 +1,90 @@
 package model;
-
-import java.awt.Graphics;
+ 
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.Map;
-
-import javax.imageio.ImageIO;
-
-import utilities.ImageProcessing;
-import utilities.Scaling;
-import controller.MapViewController;
-
-public class Avatar extends Entity implements Serializable{
-	private Inventory inventory = new Inventory();
-	private Equipment equipment = new Equipment();
-	private Location location;   // I dont think this is needed? -Juan
-	private final int INITIAL_X_LIE = 0;
-	private final int INITIAL_Y_LIE =0;
-	private BufferedImage image;
-	private final String AVATAR_IMAGE = "src/res/img/sprite.jpg";
-
-	public Avatar() {
-		this.location = new Location(INITIAL_X_LIE,INITIAL_Y_LIE);		// this is a lie
-		//occupation.initializeStats();
-	}
-	
-	public Avatar(Occupation occupation) {
-		super(occupation);
-	}
-	
-	public String getStat(String s){
-		return Integer.toString(stats.get(s).getValue());
-	}
-	
-	
-	public void move(int x,int y){ //this is not needed either -Juan
-		location.add(x,y);
-		//System.out.println(location);
-	}
-	public void loadImage(Graphics g)
-	{	
-		image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT,AVATAR_IMAGE);
-		int x= location.getX();
-		int y= location.getY();
-		g.drawImage(image,Scaling.AVATAR_WIDTH*x,Scaling.AVATAR_HEIGHT*y,null);
-		
-	}
-	
-	public BufferedImage getImage(){
-		image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT,AVATAR_IMAGE);
-		return image;
-	}
-	
-	public Inventory getInventory(){
-		return inventory;
-	}
-
-	public Equipment getEquipment() {
-		return this.equipment;
-	}
-	
-	
-	
-	
-	/*
-	//private void attack() {
-	//	
-	//}
-	//may not need to override
-	 */
-	
-	/*
-	//private void useAbility() {
-	//	
-	//}
-	//may not need to override
-	 */
-	
-	
+ 
+import utilities.*;
+ 
+public class Avatar extends Entity {
+        private Inventory inventory = new Inventory();
+        private BufferedImage image;
+        private BufferedImage[] spriteSheet;
+        private Equipment equipment = new Equipment();
+        //private final String AVATAR_IMAGE = "src/res/img/sprite.jpg";
+        private static String[] primaryStats = {"Agility", "Experience", "Hardiness",
+                                                                                        "HP", "Intellect", "Lives", "MP",
+                                                                                        "Movement", "Strength"};
+ 
+        public Avatar() {
+                //this.location = new Location(INITIAL_X_LIE,INITIAL_Y_LIE);    // this is a lie
+                //occupation.initializeStats();
+        }
+       
+        public Avatar(Occupation occupation) {
+                super(occupation);
+                String occString = occupation.getName().toUpperCase();
+                SpriteSheetUtility util = new SpriteSheetUtility(occString);
+                spriteSheet = (util.getSpriteArray());
+                //this.location = new Location(INITIAL_X_LIE, INITIAL_Y_LIE);
+        }
+       
+        public String getStat(String s){
+                return Integer.toString(stats.get(s).getValue());
+        }
+       
+        /*
+        public void move(int x,int y){ //this is not needed either -Juan
+                location.add(x,y);
+                //System.out.println(location);
+        }
+       
+        public void loadImage(Graphics g)
+        {      
+                image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT,AVATAR_IMAGE);
+                int x= location.getX();
+                int y= location.getY();
+                g.drawImage(image,Scaling.AVATAR_WIDTH*x,Scaling.AVATAR_HEIGHT*y,null);
+        }
+        */
+       
+        public BufferedImage getImage(){
+                BufferedImage imageToDisplay = spriteSheet[direction];
+                image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
+                return image;
+        }
+       
+        public Inventory getInventory(){
+                return inventory;
+        }
+       
+        public String toString() {
+                String result = "";
+                result += "Avatar:" + this.name;
+                result += "\n" + this.occupation;
+                for (String key : primaryStats) result += "\n" + this.stats.get(key);
+                result += "\nDirection:" + this.direction;
+                return result;
+        }
+        
+    	public Equipment getEquipment() {
+    		return this.equipment;
+    	}
+       
+       
+       
+       
+        /*
+        //private void attack() {
+        //     
+        //}
+        //may not need to override
+         */
+       
+        /*
+        //private void useAbility() {
+        //     
+        //}
+        //may not need to override
+         */
+       
+       
 }
