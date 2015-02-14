44e8344b44594122142ef1bac202dab1bd982334
Bernard Marger <bern422@gmail.com>
2015-02-14 15:08:47 -0500

    Implemented occupation selection. TODO, naming character.

--- Iteration1Code/src/controller/CharacterCreationController.java
+++ Iteration1Code/src/controller/CharacterCreationController.java
@@ -2,6 +2,7 @@ package controller;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,7 +14,6 @@ import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
 import model.*;
-
 import utilities.Scaling;
 import view.View;
 
@@ -27,6 +27,7 @@ public class CharacterCreationController {
 	private JLabel enterNameLabel;
 	private JTextField enterNameField;
 	private JLabel title;
+	private JLabel chooseOccupationLabel;
 	private JPanel buttons;
 	private JPanel main;
 	private JPanel back;
@@ -40,22 +41,28 @@ public class CharacterCreationController {
 		startAlchemistButton = new JButton("<html><span style='font-size:20px;'>Alchemist</span></html>");
 		startTerminatorButton = new JButton("<html><span style='font-size:20px;'>Terminator</span></html>");
 		startHunterButton = new JButton("<html><span style='font-size:20px;'>Hunter</span></html>");
-		title = new JLabel("<html><span style='font-size:40px;'><u>Character Creation</u></span><br><br><br><br><p style='font-size:10px;'>Choose an occupation to begin:</p></html>", JLabel.CENTER);
+		title = new JLabel("<html><span style='font-size:40px;'><u>Character Creation</u></span><br><br><br><br></html>", JLabel.CENTER);
 		buttons = new JPanel();
 		main = new JPanel();
 		back = new JPanel();
+		
+		enterNameLabel = new JLabel("<html><p style='font-size:20px;'>Name your character:</p><br></html>",JLabel.CENTER);
 		
 		charCreation.getCanvas().setPreferredSize(new Dimension(Scaling.CHARACTER_CREATION_WIDTH,Scaling.CHARACTER_CREATION_HEIGHT));
 		charCreation.getCanvas().setBorder(new LineBorder(Color.black, 5));
 		charCreation.getCanvas().setLayout(new BorderLayout());
 		
 		nameEntry = new JPanel();
-		enterNameLabel = new JLabel("Enter Name");
+		nameEntry.setLayout(new GridLayout(2,1));
+		Font font = new Font("Arial", Font.BOLD,20);
 		enterNameField = new JTextField("Arnold");
-		
+		enterNameField.setHorizontalAlignment(JTextField.CENTER);
+		enterNameField.setFont(font);
+
 		nameEntry.add(enterNameLabel);
 		nameEntry.add(enterNameField);
 		
+		chooseOccupationLabel = new JLabel("<html><p style='font-size:20px;'>Choose your occupation:</p></html>", JLabel.CENTER);
 		buttons.add(startHunterButton);
 		buttons.add(startTerminatorButton);
 		buttons.add(startAlchemistButton);
@@ -65,8 +72,8 @@ public class CharacterCreationController {
 		main.setLayout(new GridLayout(4,1));
 		
 		main.add(nameEntry);
+		main.add(chooseOccupationLabel);
 		main.add(buttons);
-		main.add(new JPanel());
 		main.add(back);
 		
 		charCreation.getCanvas().add(title, BorderLayout.NORTH);
