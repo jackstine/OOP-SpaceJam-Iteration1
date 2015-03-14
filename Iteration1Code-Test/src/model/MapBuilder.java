0dc2e8aefa089a95df87a702d38f1b636da97719
khushbooc <khushboochawla@ufl.edu>
2015-03-14 15:57:12 -0400

    random number generated for success and failure in skeleton class
    for skeleton a random number is generated which tells u whether the
    pickpocket was successful or not . If successful then something is added
    in inventory otherwise damage is caused to the avatar.

--- Iteration1Code-Test/src/model/MapBuilder.java
+++ Iteration1Code-Test/src/model/MapBuilder.java
@@ -111,7 +111,7 @@ public class MapBuilder{
 		
 		//NPC TEST
 		this.map[4][12].setNPC(new Merchant());
-		this.map[9][12].setNPC(new Skeleton());
+		this.map[9][12].setNPC(new Skeleton(new DamageAreaEffect(.20)));
 		this.map[14][12].setNPC(new Orc(new DamageAreaEffect(.50)));
 		return this.map;
 		
@@ -150,7 +150,7 @@ public class MapBuilder{
 		
 		//NPC TEST
 		this.map[3][13].setNPC(new Merchant());
-		this.map[6][12].setNPC(new Skeleton());
+		this.map[6][12].setNPC(new Skeleton(new DamageAreaEffect(.20)));
 		this.map[12][12].setNPC(new Orc(new DamageAreaEffect(.50)));
 		
 		return this.map;
--- Iteration1Code-Test/src/model/NpcEffectHandler.java
+++ Iteration1Code-Test/src/model/NpcEffectHandler.java
@@ -1,22 +1,25 @@
 package model;
 
+import java.util.Random;
+
 import model.Entity.Avatar;
-//import model.Entity.Merchant;
-import model.Entity.NPC;
+import model.Entity.Entity;
+import model.Entity.Merchant;
 import model.Entity.Orc;
 import model.Entity.Skeleton;
+//import model.Entity.Merchant;
 
 
 public class NpcEffectHandler {
 	private Avatar avatar;
-	
 	
 	public NpcEffectHandler(Avatar avatar){
 		
 		this.avatar=avatar;
 		
 	}
-	public void apply(NPC npc,Location avatarLocation){
+	
+	public void apply(Entity npc,Location avatarLocation){
 		
 		
 		
@@ -71,17 +74,36 @@ public class NpcEffectHandler {
 					}
 					//npc.action(avatar);
 					//tile.setItem(it);
+					int randomness=randNo();
+					if(randomness<5){
+						System.out.println("Steal Success");
+						
+					}
+					if(randomness>=5){
+						
+						npc.getAreaEffect().apply(avatar); 
+						System.out.println("Steal Failure");
+					}
+					
 				}
 		
-		//else if(npc!=null && npc instanceof Merchant){
-			//if(Integer.parseInt(avatar.getStat("Level"))>1){
+		else if(npc!=null && npc instanceof Merchant){
+			
 			
 					//MerchantView mv=new MerchantView();
 					//mv.setVisible(true);
 					//npc.action(avatar);
-					//tile.setItem(it);
 				}
-			//}
+			}
+	
+	public int randNo()
+	{
+		int x;
+		Random rand=new Random();
+		x=rand.nextInt()%10;
+		return x;
+	}
+	
 	}
 	
 	
