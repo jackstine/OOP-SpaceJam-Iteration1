d8f3ab86d7f5c03cc7f4ff835ec6e566cb6c0832
tgittelmacher <tgittelmacher@ufl.edu>
2015-03-15 15:48:33 -0400

    map added to InfluenceSet

--- Iteration1Code-Test/src/model/InfluenceSet.java
+++ Iteration1Code-Test/src/model/InfluenceSet.java
@@ -3,13 +3,15 @@ package model;
 import java.util.Collection;
 
 public abstract class InfluenceSet {
+	protected GameMap map;
 	protected Tile source;
 	protected int radius;
 	protected int direction;
 	
-	public abstract Collection<Tile> getInfluenceSet();
+	public abstract Collection<InfluenceTile> getInfluenceSet();
 	public abstract void setSource(Tile source);
 	public abstract void setRadius(int radius);
 	public abstract void setDirection(int direction);
+	public abstract void setMap(GameMap map);
 	
 }
