615abd7adf9f068bf664f40777d682d530adfcba
Jacob Lehman <jlehman@ufl.edu>
2015-03-15 15:16:32 -0400

    Skeleton classes for linear effects

---
+++ Iteration1Code-Test/src/model/LinearEffectPenetratingAttack.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Collection;
+
+public class LinearEffectPenetratingAttack extends LinearEffect{
+
+	@Override
+	public Collection<Tile> getInfluenceSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
