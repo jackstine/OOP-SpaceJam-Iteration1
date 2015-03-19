d5f9d129a1ef907951adfc0af3ea6c73caf2ef1c
jackstine <ecstaticjack@gmail.com>
2015-03-18 20:19:17 -0400

    got rid of Satan worship

--- Iteration1Code-Test/src/model/Entity/NPC.java
+++ Iteration1Code-Test/src/model/Entity/NPC.java
@@ -22,72 +22,9 @@ public abstract class NPC extends Entity {
 	public abstract SpriteSheetUtility getSpriteSheet();
 	
 	public BufferedImage getImage(){
-		if(this instanceof Orc )
-		{
-			if (this.state==1){
-				BufferedImage imageToDisplay = spriteSheet[4];
-				image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
-				return image;
-			}
-			else if (this.state==2){
-				BufferedImage imageToDisplay = spriteSheet[5];
-				image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
-				return image;
-			}
-			else if(this.state==3){
-				BufferedImage imageToDisplay = spriteSheet[2];
-				image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
-				return image;
-			}
-			else
-			{
-				BufferedImage imageToDisplay = spriteSheet[direction];
-				image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
-				return image;
-			}
-			
-		}
-		else if(this instanceof Skeleton)
-		{
-			if(this.state==1){
-				BufferedImage imageToDisplay = spriteSheet[4];
-				//System.out.println("State 1");
-				image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
-				return image;
-			}
-			else if(this.state==2){
-				BufferedImage imageToDisplay = spriteSheet[5];
-				//System.out.println("State 2");
-				image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
-				return image;
-			}
-			else if(this.state==3){
-				BufferedImage imageToDisplay = spriteSheet[2];
-				//System.out.println("State 3");
-				image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
-				return image;
-			}
-			else if(this.state==4){
-				BufferedImage imageToDisplay = spriteSheet[3];
-				//System.out.println("State 4");
-				image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
-				return image;
-			}
-			else
-			{
-				BufferedImage imageToDisplay = spriteSheet[direction];
-				image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
-				return image;
-			}
-			
-		}
-		
-		else {
 			BufferedImage imageToDisplay = spriteSheet[direction];
 			image= ImageProcessing.scaleImage(Scaling.AVATAR_WIDTH, Scaling.AVATAR_HEIGHT, imageToDisplay);
 			return image;
-			
-		}
 	}
 	public void setState(int state){
 		this.state=state;
