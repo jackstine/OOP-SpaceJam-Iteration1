767b2f8767f932ef6f7e0d4553d0ccccd3f64063
tgittelmacher <tgittelmacher@ufl.edu>
2015-03-22 18:34:00 -0400

    Major bug fix
    Suprise motherfucker is louder

--- Iteration1Code-Test/src/model/TrapDamageApplication.java
+++ Iteration1Code-Test/src/model/TrapDamageApplication.java
@@ -8,6 +8,6 @@ public class TrapDamageApplication {
 	
 	public static void apply(Entity entity, Trap trap){
 		entity.setStatValue("HP",(int)(entity.getStatValue("HP") - trap.getDamage())); 
-	
+		SoundEffect e = new HitTrapSoundEffect();
 	}
 }
