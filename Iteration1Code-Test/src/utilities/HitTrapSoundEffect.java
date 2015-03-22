ea9b119a52f7c6e8bf3d22838f09f2ab70a9976b
Jacob Lehman <jlehman@ufl.edu>
2015-03-22 17:29:30 -0400

    No description necessary

---
+++ Iteration1Code-Test/src/utilities/HitTrapSoundEffect.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class HitTrapSoundEffect extends SoundEffect{
+
+	@Override
+	protected String getFilePath() {
+		// TODO Auto-generated method stub
+		return "src/res/audio/surprise_motherfucker.wav";
+	}
+
+}
