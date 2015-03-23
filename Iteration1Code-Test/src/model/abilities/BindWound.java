8b72c8cb9f1053aa3a8dbd2980e58d971c880587
jackstine <ecstaticjack@gmail.com>
2015-03-22 20:32:34 -0400

    Binding

--- Iteration1Code-Test/src/model/abilities/BindWound.java
+++ Iteration1Code-Test/src/model/abilities/BindWound.java
@@ -1,19 +1,22 @@
 package model.abilities;
 
 import model.entity.Entity;
+import model.entity.EntityEffectHandler;
 
 public class BindWound extends Spell{
+	private int heal = 50;
 
 	public BindWound(Entity entity) {
 		super(entity);
 	}
 
 	public boolean able() {
-		return false;
+		return true;
 	}
 
 	@Override
 	protected void doTheSpell(Entity entityToAffect) {
+		EntityEffectHandler.applyHeal(entityToAffect, heal);
 	}
 
 	@Override
